cf155ab5c800445e7a9316d5e23669205c3bcea7
Rene <rene.treier@gmail.com>
2021-09-13 19:26:55 +0300

    Klassidiagrarammide algus/BusinessModel

---
+++ Domain/Location.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Domain
+{
+    public class Location
+    {
+        public string Address
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public int Phone
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public int LocationID
+        {
+            get => default;
+            set
+            {
+            }
+        }
+    }
+}
---
+++ Domain/Occupation.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Domain
+{
+    public class Occupation
+    {
+        public int OccupationID
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public string OccupationName
+        {
+            get => default;
+            set
+            {
+            }
+        }
+    }
+}

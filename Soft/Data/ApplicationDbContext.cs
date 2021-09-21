e623e721483d598577198c49b9dee4adb623e88e
Rene <rene.treier@gmail.com>
2021-09-21 10:24:27 +0300

    ApplicationDBcontext ver1

--- Soft/Data/ApplicationDbContext.cs
+++ Soft/Data/ApplicationDbContext.cs
@@ -1,13 +1,27 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Data;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Soft.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+        public ApplicationDbContext() : this(
+            new DbContextOptionsBuilder<ApplicationDbContext>().Options)
+        { }
+
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-            : base(options)
+            : base(options) { }
+        public DbSet<AddressData> Addresses { get; set; }
+
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<AddressData>().ToTable("Address");
+         
         }
     }
 }

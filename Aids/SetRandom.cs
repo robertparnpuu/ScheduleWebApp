b895f3242955705125b40f9e3ad683803afad3df
Rene <rene.treier@gmail.com>
2021-09-22 20:17:03 +0300

    Created AssemblyInfo for each project and took some Aids with their tests from ISA2 project

---
+++ Aids/SetRandom.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+
+namespace Aids
+{
+    public static class SetRandom
+    {
+
+        public static void Values(object o)
+        {
+            if (o is null) return;
+            if (o is IList list) SetValuesForList(list);
+            else SetValuesForProperties(o);
+        }
+        private static void SetValuesForProperties(object o)
+        {
+            if (o is null) return;
+            var t = o.GetType();
+            var properties = t.GetProperties();
+            foreach (var p in properties)
+            {
+                if (!p.CanWrite) continue;
+                if (p.PropertyType.Name == t.Name) continue;
+                var v = GetRandom.ValueOf(p.PropertyType);
+                p.SetValue(o, v);
+            }
+        }
+
+        private static void SetValuesForList(IList l)
+        {
+            if (l is null) return;
+            var t = GetListElementsType(l);
+            for (var c = 0; c <= GetRandom.UInt8(3, 5); c++)
+            {
+                var v = GetRandom.ValueOf(t);
+                l.Add(v);
+            }
+        }
+        private static Type GetListElementsType(IList list)
+        {
+            return Safe.Run(() => {
+                var t = list.GetType();
+                var types =
+                    (from method in t.GetMethods()
+                     where method.Name == "get_Item"
+                     select method.ReturnType
+                    ).Distinct().ToArray();
+                return types[0];
+            }, null);
+        }
+    }
+}
---
+++ Tests/Aids/GetEnumTests.cs
@@ -0,0 +1,73 @@
+﻿using Aids;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+
+namespace Tests.Aids
+{
+    [TestClass]
+    public class GetEnumTests
+    {
+        private enum TestEnum
+        {
+            Undefined = 0,
+            First = 1,
+            Second = 123
+        }
+        [TestMethod]
+        public void CountTest()
+            => Assert.AreEqual(3, GetEnum.Count<TestEnum>());
+        [TestMethod]
+        public void CountTestByType()
+            => Assert.AreEqual(3, GetEnum.Count(typeof(TestEnum)));
+
+        [TestMethod]
+        public void ValueByIndexTest()
+            => Assert.AreEqual(TestEnum.First, GetEnum.ValueByIndex<TestEnum>(1));
+        [TestMethod]
+        public void ValueByIndexTestByType()
+            => Assert.AreEqual(TestEnum.Second, GetEnum.ValueByIndex(typeof(TestEnum), 2));
+
+        [TestMethod]
+        public void ValueByValueTest()
+            => Assert.AreEqual(TestEnum.First, GetEnum.ValueByValue<TestEnum>(1));
+        [TestMethod]
+        public void ValueByValueTestByType()
+            => Assert.AreEqual(TestEnum.Second, GetEnum.ValueByValue(typeof(TestEnum), 123));
+
+        [TestMethod]
+        public void CountTestWrongType()
+            => Assert.AreEqual(-1, GetEnum.Count<string>());
+        [TestMethod]
+        public void CountTestByTypeWrongType()
+            => Assert.AreEqual(-1, GetEnum.Count(typeof(int)));
+
+        [TestMethod]
+        public void ValueByIndexTestWrongIndex()
+            => Assert.AreEqual(TestEnum.Undefined, GetEnum.ValueByIndex<TestEnum>(100));
+        [TestMethod]
+        public void ValueByIndexTestByTypeWrongIndex()
+            => Assert.AreEqual(TestEnum.Undefined, GetEnum.ValueByIndex(typeof(TestEnum), 100));
+
+        [TestMethod]
+        public void ValueByValueTestWrongIndex()
+            => Assert.AreEqual(TestEnum.Undefined, GetEnum.ValueByValue<TestEnum>(100));
+        [TestMethod]
+        public void ValueByValueTestByTypeWrongIndex()
+            => Assert.AreEqual(TestEnum.Undefined, GetEnum.ValueByValue(typeof(TestEnum), 100));
+
+        [TestMethod]
+        public void ValueByIndexTestWrongType()
+            => Assert.AreEqual(null, GetEnum.ValueByIndex<string>(100));
+        [TestMethod]
+        public void ValueByIndexTestByTypeWrongType()
+            => Assert.AreEqual(null, GetEnum.ValueByIndex(typeof(string), 100));
+
+        [TestMethod]
+        public void ValueByValueTestWrongType()
+            => Assert.AreEqual(0, GetEnum.ValueByValue<int>(100));
+        [TestMethod]
+        public void ValueByValueTestByTypeWrongType()
+            => Assert.AreEqual(0, GetEnum.ValueByValue(typeof(int), 100));
+
+    }
+}

87bbe21eac503c26ea153e80143abc449276f978
Kalle Pilli <kapill@ttu.ee>
2021-10-08 13:30:03 +0300

    ShowHtml, EditorHtml

---
+++ Core/Extensions/EditorHtml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Core.Extensions {
+    public static class EditorHtml {
+
+        public static IHtmlContent Editor<TModel, TResult>(
+            this IHtmlHelper<TModel> h, Expression<Func<TModel, TResult>> e)
+            => Editor(h, e, e);
+        public static IHtmlContent Editor<TModel, TResult1, TResult2>(
+            this IHtmlHelper<TModel> h, Expression<Func<TModel, TResult1>> label,
+             Expression<Func<TModel, TResult2>> value) {
+            var n = h.DisplayNameFor(label);
+            return Editor(h, value, n);
+        }
+        public static IHtmlContent Editor<TModel, TResult>(this IHtmlHelper<TModel> h,
+            Expression<Func<TModel, TResult>> e, string displayName) {
+            var s = HtmlStrings(h, e, displayName);
+            return new HtmlContentBuilder(s);
+        }
+        internal static List<object> HtmlStrings<TModel, TResult>(IHtmlHelper<TModel> h,
+            Expression<Func<TModel, TResult>> e, string displayName) {
+            return new()
+            {
+                new HtmlString("<dd class=\"col-sm-2\">"),
+                h.Raw(displayName),
+                new HtmlString("</dd>"),
+                new HtmlString("<dd class=\"col-sm-10\">"),
+                h.EditorFor(e, new {htmlAttributes = new {@class = "form-control"}}),
+                h.ValidationMessageFor(e, "", new {@class = "text-danger"}),
+                new HtmlString("</dd>")
+            };
+        }
+    }
+}
---
+++ Core/Extensions/ShowHtml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text.Encodings.Web;
+using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Core.Extensions {
+    public static class ShowHtml {
+        public static IHtmlContent Show<TModel, TResult>(
+            this IHtmlHelper<TModel> h,
+            Expression<Func<TModel, TResult>> e
+        ) => Show(h, e, e);
+
+        public static IHtmlContent Show<TModel, TResult1>(
+            this IHtmlHelper<TModel> h,
+            Expression<Func<TModel, TResult1>> label,
+            object value) {
+            var labelStr = h.DisplayNameFor(label);
+            return h.Show(labelStr, value.ToString());
+        }
+        public static IHtmlContent Show<TModel, TResult1, TResult2>(
+            this IHtmlHelper<TModel> h,
+            Expression<Func<TModel, TResult1>> label,
+            Expression<Func<TModel, TResult2>> value = null) {
+            var labelStr = h.DisplayNameFor(label);
+            var valueStr = (value is null) 
+                ? GetValue(h, label) 
+                : GetValue(h, value);
+            return h.Show(labelStr, valueStr);
+        }
+        public static IHtmlContent Show<TModel>(
+            this IHtmlHelper<TModel> h, string label, string value) {
+            if (h == null) throw new ArgumentNullException(nameof(h));
+            var s = HtmlStrings(h, label, value);
+            return new HtmlContentBuilder(s);
+        }
+        internal static List<object> HtmlStrings<TModel>(
+            IHtmlHelper<TModel> h, string label, string value) {
+            return HtmlStrings(
+                h.Raw(label),
+                h.Raw(value)
+                );
+        }
+        internal static string GetValue<TModel, TResult>(IHtmlHelper<TModel> h, Expression<Func<TModel, TResult>> e) {
+            var value = h.DisplayFor(e);
+            var writer = new System.IO.StringWriter();
+            value.WriteTo(writer, HtmlEncoder.Default);
+            return writer.ToString();
+        }
+        public static List<object> HtmlStrings(object label, object value) {
+            return new (){
+                new HtmlString("<dd class=\"col-sm-2\">"),
+                label,
+                new HtmlString("</dd>"),
+                new HtmlString("<dd class=\"col-sm-10\">"),
+                value,
+                new HtmlString("</dd>")
+            };
+        }
+    }
+}
+
+
+
+
+
+

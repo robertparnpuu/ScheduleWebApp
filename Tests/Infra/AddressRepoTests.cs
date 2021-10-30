6a14ceeaa6b4d91b33dc2381f916a455ece435d1
renetreier <78070145+renetreier@users.noreply.github.com>
2021-10-30 15:09:03 +0300

    Some Addresstests

---
+++ Tests/Infra/AddressRepoTests.cs
@@ -0,0 +1,55 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Aids;
+using Data;
+using Domain;
+using Infra;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests.Infra
+{
+    [TestClass]
+    public class AddressRepoTests
+    {
+        protected AddressRepo CreateObject()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+            .UseInMemoryDatabase("TestDb").Options;
+            var c = new ApplicationDbContext(options);
+            return new AddressRepo(c);
+        }
+
+        public AddressRepo mockRepo;
+        public Address aEntity;
+        public AddressData aData;
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            mockRepo = CreateObject();
+            aEntity = new Address(GetRandom.ObjectOf<AddressData>());
+        }
+        [TestMethod]
+        public async Task AddAsyncTest()
+        {
+            Assert.IsInstanceOfType(mockRepo, typeof(AddressRepo));
+            Assert.IsInstanceOfType(aEntity, typeof(Address));
+            var expected = ToData(aEntity);
+            await mockRepo.AddAsync(aEntity);
+            ArePropertiesEqual(expected, aEntity.Data);
+        }
+
+        protected static void ArePropertiesEqual<T>(T expected, T actual, params string[] exceptProperties)
+        {
+            foreach (var p in typeof(T).GetProperties())
+            {
+                var expectedValue = p.GetValue(expected);
+                var actualValue = p.GetValue(actual);
+                if (exceptProperties.Contains(p.Name)) Assert.AreNotEqual(expectedValue, actualValue);
+                else Assert.AreEqual(expectedValue, actualValue);
+            }
+        }
+        public AddressData ToData(Address e) => e?.Data ?? new AddressData();
+        public  Address ToEntity(AddressData d) => new(d);
+    }
+}
---
+++ Tests/Infra/Common/BaseRepoTests.cs
@@ -0,0 +1,49 @@
+﻿//using Aids;
+//using Core;
+//using Data.Common;
+//using Domain.Common;
+//using Infra;
+//using Infra.Common;
+//using Microsoft.EntityFrameworkCore;
+//using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+//namespace Tests.Infra.Common
+//{
+//    public abstract class BaseRepoTests<TRepo,TData,TEntity>
+//    where TRepo:BaseRepo<TData,TEntity>
+//    where TData : BaseEntityData, IBaseEntityData, new()
+//    where TEntity : BaseEntity<TData>, IBaseEntity, new()
+//    {
+//        protected TEntity obj;
+//        protected TRepo mockRepo;
+//        protected abstract TRepo CreateRepo(ApplicationDbContext c);
+
+//        protected BaseRepo<TData,TEntity> CreateObject()
+//        {
+//            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+//            .UseInMemoryDatabase("TestDb").Options;
+//            var c = new ApplicationDbContext(options);
+//            return new TestRepo<TData,TEntity>(c);
+//        }
+//        [TestInitialize]
+//        public void TestInitialize()
+//        {
+//            obj = CreateEntityObject();
+//            mockRepo = CreateObject();
+//        }
+//        protected virtual TEntity CreateEntityObject() => GetRandom.ObjectOf<TEntity>();
+//    }
+//    public class TestRepo<TData, TEntity> : BaseRepo<TData,TEntity>
+//    where TData : BaseEntityData, IBaseEntityData, new()
+//    where TEntity : BaseEntity<TData>, IBaseEntity, new()
+//    {
+//        public TestRepo(ApplicationDbContext c = null)
+//        : base(c, null)
+//        {
+//        }
+
+//        public override TEntity ToEntity(TData d) => new();
+//        public override TData ToData(TEntity e) => e.Data;
+//    }
+    
+//}

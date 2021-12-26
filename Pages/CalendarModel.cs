2bbef5d73e7d1a7cdef08b3111f65ea325806128
Robert Pärnpuu <rparnp@ttu.ee>
2021-12-26 13:43:35 +0200

    FullCalendar files work through a link, trying to make references to local js files work

---
+++ Pages/CalendarModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace PageModels
+{
+    [Authorize(Roles = "Admin, Viewer")]
+    public class CalendarModel : PageModel
+    {
+
+        public IActionResult OnGetIndex()
+        {
+            return Page();
+        }
+    }
+}

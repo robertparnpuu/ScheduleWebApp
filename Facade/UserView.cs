6088ab1e695d268ff2abe885655642100f402e2e
Kalle Pilli <kapill@ttu.ee>
2021-12-29 14:26:35 +0200

    Can assign person to user

---
+++ Facade/UserView.cs
@@ -0,0 +1,10 @@
+﻿namespace Facade
+{
+    public class UserView
+    {
+        public string userName { get; set; } 
+        public string password { get; set; }
+        public string userPersonName { get; set; }
+        public string userPersonId { get; set; }
+    }
+}
